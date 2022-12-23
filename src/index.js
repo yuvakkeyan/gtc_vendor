fb07dfdf7215bed4c8ff4083aecfd8fdd273265e
Nathiya <nadhuswt@gmail.com>
2022-12-23 21:21:31 +0530

    registration root added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/reg', function (req, res) {
+  res.send('Registration Page');
+})
+
 app.get('/', function (req, res) {
   res.send('Landing Page');
 })
